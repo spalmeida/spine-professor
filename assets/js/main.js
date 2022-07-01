b35df9d35b768e002ba63a8b84108a27cc2481b4
Samuel <samuelprado.a@gmail.com>
2022-07-01 14:46:35 -0300

    test: animations buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
-let animationState = "idle";
 /*
 |--------------------------------------------------------------------------
 | create the pixi app
 |--------------------------------------------------------------------------
 */
 
+//let animationState = "idle";
+let animationState = "Clic_idle";
 const app = new PIXI.Application({
-  transparent: true,
+  backgroundAlpha: 0,
   width: 300,
   height: 300,
 });
@@ -14,11 +15,11 @@ document.getElementById("chatProfessor").appendChild(app.view);
 
 /*
 |--------------------------------------------------------------------------
-| load spine data
+| load spine data SPINE VERSION 3.8
 |--------------------------------------------------------------------------
 */
 app.loader
-  .add("professor", "professor/3.8/Professor_Spine.json")
+  .add("professor", "spines/professor/Professor_Spine.json")
   .load(onAssetsLoaded);
 app.stage.interactive = true;
 
@@ -27,11 +28,6 @@ app.stage.interactive = true;
 | assets loaded, start the animation
 |--------------------------------------------------------------------------
 */
-
-function disableEvents(e) {
-  e.stopPropagation();
-  e.preventDefault();
-}
 
 function onAssetsLoaded(loader, res) {
   const professorSpine = new PIXI.spine.Spine(res.professor.spineData);
@@ -47,14 +43,11 @@ function onAssetsLoaded(loader, res) {
   app.stage.addChild(professorSpine);
 
   // @AnimationsNames ['Clic', 'Clic_idle', 'idle', 'Volta'];
-  //const singleAnimations = ["idle", "Clic", "Clic_idle", "Volta"];
-  //const allAnimations = [].concat(singleAnimations, loopAnimations);
   const loopAnimations = ["idle", "Clic_idle"];
-  professorSpine.state.setAnimation(
-    0,
-    animationState,
-    loopAnimations.includes(animationState)
-  );
+
+  // default animation started
+  addButtons("0");
+  setAnimation(animationState, professorSpine, loopAnimations);
 
   /*
   |--------------------------------------------------------------------------
@@ -90,8 +83,6 @@ function onAssetsLoaded(loader, res) {
       document.removeEventListener("click", disableEvents, true);
     }, 2000);
   });
-
-  console.log(animationState);
 }
 
 function setAnimation(animationName, spine, loopAnimations, time = 0) {
@@ -102,20 +93,47 @@ function setAnimation(animationName, spine, loopAnimations, time = 0) {
       loopAnimations.includes(animationName)
     );
   }, time);
+  console.log("Animation Name Active: " + animationState);
 }
 
 function addButtons(time = 700) {
   const buttons = document.getElementById("chat-professor-buttons");
+  let buttonsBox = document.querySelector(".chat-professor-button");
+  //buttonsBox.classList.toggle("animate__fadeInDown");
 
   setTimeout(() => {
-    buttons.innerHTML = "<b>TESTE</b>";
+    buttons.innerHTML = `
+      <div class="chat-professor-button animate__animated">
+        <div class="options">
+          <div class="option">
+            <div class="option-icon">
+             <i class="fa-brands fa-whatsapp"></i>
+            </div>
+            <div class="option-text">
+             <span>whatsapp</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    document
+      .querySelector(".chat-professor-button")
+      .classList.toggle("animate__fadeInDown");
   }, time);
 }
 
 function removeButtons(time = 700) {
   const buttons = document.getElementById("chat-professor-buttons");
-
+  document
+    .querySelector(".chat-professor-button")
+    .classList.toggle("animate__fadeOutUp");
   setTimeout(() => {
     buttons.innerHTML = "";
   }, time);
+}
+
+// DISABLE CLICKS
+function disableEvents(e) {
+  e.stopPropagation();
+  e.preventDefault();
 }
